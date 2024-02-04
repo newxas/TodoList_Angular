8c75ad66876745a0466e6994b484bdd3ae3bc772
newxas <ismax.026@gmail.com>
2024-02-03 20:58:13 -0700

    Practice Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TodoList_Angular';
+  todos: Todo[] = [];
+  newTodo: string;
+  LocalDate: string
+
+  ngOnInit() {
+    this.LocalDate = new Date().toLocaleDateString('en-US')
+  }
+
+  saveTodo()
+  {
+    if(this.newTodo)
+    {
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.IsCompleted = true;
+      this.todos.push(todo);
+      this.newTodo = ''
+    }
+  }
+
+  removeTodo(id: number)
+  {
+    this.todos = this.todos.filter((v, i)=> i != id)
+  }
+  
+  done(id: number)
+  {
+    this.todos[id].IsCompleted = !this.todos[id].IsCompleted;
+  }
 }
